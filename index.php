969ec40d2c3e2d76df9281f364292eb648726d08
AHaberl <andreashaberl@gmx.net>
2016-02-14 16:57:07 +0100

    Move logic from index to page class

--- index.php
+++ index.php
@@ -1,62 +1,17 @@
 <?php
 
-include "site.class.php";
-include "navigation.class.php";
+require "page.class.php";
 
 
 if(isset($_GET["id"])){
 	$id = $_GET["id"];
 } else {
-	$id = 4;
+	$id = 1;
 }
 
-$site = new Site($id);
-echo $site->getName();
-print("<br>");
-echo $site->getParent();
-print("<br>");
+$page = new Page($id);
 
-$topNavi = new Navigation("topnavi", -1);
+print $page->__toString();
 
-print("topnavi: <br>");
-
-echo($topNavi->getName());
-print("<br>");
-
-foreach($topNavi->getItems() as $item) {
-	print($item . " target: " . $item->getTarget());
-	print("<a href='index.php?id=" . $item->getTarget() . "'>" . $item . "</a>");
-	print("<br>");
-}
-
-print("<br><br>");
-
-if($site->getParent() == -1){	
-	print("Second Navi: <br>");
-	$sideNavi = new Navigation("sidenavi", $site->getId());
-	echo($sideNavi->getName());
-	print("<br>");
-	foreach($sideNavi->getItems() as $item) {
-		print($item . " target: " . $item->getTarget());
-		print("<a href='index.php?id=" . $item->getTarget() . "'>" . $item . "</a>");
-		print("<br>");
-	}
-	print("<br>");
-} else {
-	print("Second Navi: <br>");
-	$sideNavi = new Navigation("sidenavi", $site->getParent());
-	echo($sideNavi->getName());
-	print("<br>");
-	foreach($sideNavi->getItems() as $item) {
-		print($item . " target: " . $item->getTarget());
-		print("<a href='index.php?id=" . $item->getTarget() . "'>" . $item . "</a>");
-		print("<br>");
-	}
-	print("<br>");
-}
-
-print("site: <br>");
-
-echo($site->getContent());
 
 ?>
--- page.class.php
+++ page.class.php
@@ -2,19 +2,49 @@
 
 require "site.class.php";
 require "navigation.class.php";
-
+require "template/sgi_template/index.php";
 
 class Page {
-	
+
+	private $navi_left_top = "<img src='template/sgi_template/Bilder/navi.png' alt='Navigation' id='naviheader'/> <ul id='ul_navi_2'>";
+	private $navi_left_botton = "</ul><img src='template/sgi_template/Bilder/navigation_bottom.png' alt=' ' />";	
+
 	private $site;
 	private $navi_top;
 	private $navi_left;
 	private $template;
 
 	public function __construct($id){
+		$this->template = new SgiTemplate();
 		$this->site = new Site($id);
-		$this->navi_top = new Navigation("topnavi", -1);
+		$this->navi_top = new Navigation("topnavi", -1, "top");
+
+		if($this->site->getParent() == -1){
+			$this->navi_left = new Navigation("leftnavi", $this->site->getId(), "left");
+		} else {
+			$this->navi_left = new Navigation("leftnavi", $this->site->getParent(), "left");
+		}
 	}	
+
+
+	public function __toString(){
+
+		$content = array();
+		$content["title"] = $this->site->getName();
+		$content["navi_top"] = $this->navi_top->__toString();
+		
+		if(!$this->navi_left->isEmpty()){
+			$content["navi_left"] = $this->navi_left_top . $this->navi_left->__toString() . $this->navi_left_botton;
+		} else {
+			$content["navi_left"] = "";
+		}
+
+		$content["textarea"] = $this->site->getContent();
+
+		$this->template->setContent($content);
+		
+		return $this->template->__toString();		
+	}
 
 
 }
